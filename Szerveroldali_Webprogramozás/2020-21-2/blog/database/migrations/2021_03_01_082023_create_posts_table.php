e7aae452de8d741a9f09808abc8aec48fde58b7c
Robearr <54249226+Robearr@users.noreply.github.com>
2021-03-07 20:39:14 +0100

    adatbáziskezelés - 4. gyakorlat

---
+++ Szerveroldali_Webprogramozás/2020-21-2/blog/database/migrations/2021_03_01_082023_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title', 48);
+            $table->text('text');
+            $table->boolean('disable_comments')->nullable()->default(false);
+            $table->boolean('hide_post')->nullable()->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
